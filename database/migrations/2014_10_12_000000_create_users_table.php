e1e010260111370ee289d4c6ab4b502e7c549c9d
Fabyan Kindarya <fkindarya@gmail.com>
2021-10-04 15:13:22 +0700

    Sanctum Installation

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('image')->nullable();
+            $table->boolean('check');
             $table->rememberToken();
             $table->timestamps();
         });
