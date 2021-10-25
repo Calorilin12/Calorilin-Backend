9d71ee5e7c3aa865e7a21708dbb1d5f61a35e61a
Fabyan Kindarya <fkindarya@gmail.com>
2021-10-25 19:52:17 +0700

    vendor publish

--- routes/api.php
+++ routes/api.php
@@ -21,35 +21,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/users', [UserController::class, 'users']);
 
 Route::middleware(['auth:sanctum'])->group(function ()
 {
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-    Route::get('users', [UserController::class, 'users']);
-    Route::put('users/update/{id}', [UserController::class, 'update']);
+    // Route::get('/users', [UserController::class, 'users']);
+    Route::put('/users/update/{id}', [UserController::class, 'update']);
 
-    Route::get('food', [FoodController::class,'foods']);
-    Route::post('food', [FoodController::class, 'create']);
-    Route::put('food/update/{id}', [FoodController::class, 'update']);
-    Route::delete('food/delete/{id}', [FoodController::class, 'delete']);
+    Route::get('/food', [FoodController::class,'foods']);
+    Route::post('/food', [FoodController::class, 'create']);
+    Route::put('/food/update/{id}', [FoodController::class, 'update']);
+    Route::delete('/food/delete/{id}', [FoodController::class, 'delete']);
 
-    Route::get('food-material', [FoodMaterialController::class, 'food_materials']);
-    Route::post('food-material', [FoodMaterialController::class, 'create']);
-    Route::put('food-material/update/{id}', [FoodMaterialController::class, 'update']);
-    Route::delete('food-material/delete/{id}', [FoodMaterialController::class, 'delete']);
+    Route::get('/food-material', [FoodMaterialController::class, 'food_materials']);
+    Route::post('/food-material', [FoodMaterialController::class, 'create']);
+    Route::put('/food-material/update/{id}', [FoodMaterialController::class, 'update']);
+    Route::delete('/food-material/delete/{id}', [FoodMaterialController::class, 'delete']);
 
-    Route::post('control-calory', [ControlCaloryController::class, 'create']);
-    Route::get('control-calory', [ControlCaloryController::class, 'control_calories']);
-    Route::put('control-calory/update/{id}', [ControlCaloryController::class, 'update']);
-    Route::delete('control-calory/delete/{id}', [ControlCaloryController::class, 'delete']);
+    Route::post('/control-calory', [ControlCaloryController::class, 'create']);
+    Route::get('/control-calory', [ControlCaloryController::class, 'control_calories']);
+    Route::put('/control-calory/update/{id}', [ControlCaloryController::class, 'update']);
+    Route::delete('/control-calory/delete/{id}', [ControlCaloryController::class, 'delete']);
 
-    Route::post('food-schedule', [FoodScheduleController::class, 'create']);
-    Route::get('food-schedule', [FoodScheduleController::class, 'food_schedules']);
-    Route::put('food-schedule/update/{id}', [FoodScheduleController::class, 'update']);
-    Route::delete('food-schedule/delete/{id}', [FoodScheduleController::class, 'delete']);
+    Route::post('/food-schedule', [FoodScheduleController::class, 'create']);
+    Route::get('/food-schedule', [FoodScheduleController::class, 'food_schedules']);
+    Route::put('/food-schedule/update/{id}', [FoodScheduleController::class, 'update']);
+    Route::delete('/food-schedule/delete/{id}', [FoodScheduleController::class, 'delete']);
 });
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
